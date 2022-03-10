9ee045482f898dc7e7324fdc29a6813e4b7649d4
Jacob Kelly <bananacreamkelly@gmail.com>
2022-03-10 14:14:06 +0100

    ..

--- client/src/components/AllBooksList.js
+++ client/src/components/AllBooksList.js
@@ -32,7 +32,7 @@ export default function AllBooksList() {
 
 			<h1>Book Shelf</h1>
       <div className='listDiv'>
-			{books.map(book => 
+			{books && books.map(book => 
       
             <ul>
             <li>{book.fiction ? <p>Fiction</p> : <p>Non-Fiction</p>}</li> 
