94cbda3e0c605492c39d68196747f0cf9374e53c
Jacob Kelly <bananacreamkelly@gmail.com>
2022-03-10 12:31:53 +0100

    .

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://Jacob:books@cluster0.8ttmp.mongodb.net/booksdatabase?retryWrites=true&w=majority";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/library";
 
 mongoose
   .connect(MONGO_URI)
